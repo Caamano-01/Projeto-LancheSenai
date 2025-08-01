db260d372bfa3fe33a0a6586cf16ae7615afb55f
Caamano-01 <isabelecaamano87@gmail.com>
2025-08-01 17:20:29 -0300

    Finalizado

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+// Funções para navegação entre páginas
+function irParaCardapio() {
+    window.location.href = 'cardapio.html';
+}
+
+function fazerPedido() {
+    window.location.href = 'pedido.html';
+}
